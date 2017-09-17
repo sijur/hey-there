e4e61ed12028499691ac1f5b5f437b5a016fd1f9
Steven O'Driscoll <slodriscoll@gmail.com>
2017-09-17 12:32:16 -0600

    Altering, and adding files

---
+++ scripts/say_hey.class.php
@@ -0,0 +1,19 @@
+<?php
+
+class HeyThere
+{
+    public function __construct($name)
+    {
+        $name = "Steven";
+    }
+
+    public function setup()
+    {
+        $this->sayHey();
+    }
+
+    private function sayHey()
+    {
+        echo "<"
+    }
+}
